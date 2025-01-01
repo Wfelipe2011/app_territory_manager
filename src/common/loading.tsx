c1091c72866e8bf7cc16f72d82d41b1864cf18d2
Wilson Felipe da Silva <wfelipe2011@gmail.com>
2025-01-01 12:16:32 -0300

    chore: update project version to v1.10.0 in loading component

--- src/common/loading.tsx
+++ src/common/loading.tsx
@@ -22,7 +22,7 @@ export const RootModeScreen = ({ children, mode = 'loading' }: {
   children: React.ReactNode;
   mode: Mode;
 }) => {
-  const projectVersion = 'v1.1.0';
+  const projectVersion = 'v1.10.0';
   return (
     <div className='m-auto max-w-[1880px] '>
       <Toaster />
